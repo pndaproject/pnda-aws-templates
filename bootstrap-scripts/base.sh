74f4f0f2f83315a6912b8accac16c9cd489b03bf
trsmith2 <trsmith2@cisco.com>
2017-02-02 08:40:15 +0000

    Log IP addresses used by PNDA

--- bootstrap-scripts/base.sh
+++ bootstrap-scripts/base.sh
@@ -9,6 +9,23 @@
 
 set -e
 
+
+# Log the global scope IP connection.
+cat > /etc/rsyslog.d/10-iptables.conf <<EOF
+:msg,contains,"[iplog] " /var/log/iptables.log
+STOP
+EOF
+sudo service rsyslog restart
+iptables -N LOGGING
+iptables -A OUTPUT -j LOGGING
+## Accept all local scope IP packets.
+  ip address show  | awk '/inet /{print $2}' | while IFS= read line; do \
+iptables -A LOGGING -d  $line -j ACCEPT
+  done
+## And log all the remaining IP connections.
+iptables -A LOGGING -j LOG --log-prefix "[iplog] " --log-level 7 -m state --state NEW
+
+
 DISTRO=$(cat /etc/*-release|grep ^ID\=|awk -F\= {'print $2'}|sed s/\"//g)
 
 if [ "x$DISTRO" == "xubuntu" ]; then
