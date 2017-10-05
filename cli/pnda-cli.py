de8d71744b38e6c91f1d2e0e863e203e158d5b6c
James Clarke <jamclar2@cisco.com>
2017-10-05 15:43:38 +0100

    Update console output for pnda-cli
    Update console output for pnda-cli to match style of the existing messages.

--- cli/pnda-cli.py
+++ cli/pnda-cli.py
@@ -452,7 +452,7 @@ def create(template_data, cluster, flavor, keyname, no_config_check, dry_run, br
     ssh(['(sudo salt -v --log-level=debug --timeout=120 --state-output=mixed "*" state.highstate queue=True 2>&1) | tee -a pnda-salt.log; %s' % THROW_BASH_ERROR,
          '(sudo CLUSTER=%s salt-run --log-level=debug state.orchestrate orchestrate.pnda 2>&1) | tee -a pnda-salt.log; %s' % (cluster, THROW_BASH_ERROR),
          '(sudo salt "*-%s" state.sls hostsfile 2>&1) | tee -a pnda-salt.log; %s' % (bastion, THROW_BASH_ERROR)], cluster, saltmaster_ip)
-    CONSOLE.info("Nodes may reboot due to kernel upgrade, wait for few minutes")
+    CONSOLE.info("Waiting for instances to reboot following kernel upgrade. Expect this to take a few minutes.")
     time.sleep(60)
     wait_for_host_connectivity([instance_map[h]['private_ip_address'] for h in instance_map], cluster)
     
